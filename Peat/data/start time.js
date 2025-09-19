029c736c3bc6d7b056206c3bce5cb4e532f9e17d
juicein <77275840+juicein@users.noreply.github.com>
2025-09-20 03:09:58 +0700

    Create start time.js

---
+++ Peat/data/start time.js
@@ -0,0 +1,16 @@
+story = [
+  {
+    name: "旁白",
+    text: "在一个宁静的夜晚，故事开始了……",
+    bg: "assets/bg/night.jpg",
+    char: "",
+    voice: "assets/voices/narration1.mp3"
+  },
+  {
+    name: "小雅",
+    text: "嗨，你终于来了！",
+    bg: "assets/bg/room.jpg",
+    char: "assets/characters/xiaoya_happy.png",
+    voice: "assets/voices/xiaoya1.mp3"
+  }
+];
