15d5fd9cab79da793ea3971635dc1e0911d714b7
juicein <77275840+juicein@users.noreply.github.com>
2025-11-30 19:05:16 +0700

    Update script.js

--- flights_map/script.js
+++ flights_map/script.js
@@ -25,7 +25,7 @@ let flightMarkers = {};
 let flightLines = {};
 let highlightedKey = null; // track highlighted flight
 
-const PLANE_IMG = "https://i.imgur.com/4bZtV3y.png"; // 你确认的图片：机头向上（北）
+const PLANE_IMG = "../image/flight.png"; // 你确认的图片：机头向上（北）
 
 // ============== 工具函数 ==============
 function getFlightIDFromURL() {
