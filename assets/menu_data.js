8271c0e56fba6026442cb1a1d99e7fa34ef27ace
juicein <77275840+juicein@users.noreply.github.com>
2025-12-28 02:54:35 +0700

    Update menu_data.js

--- assets/menu_data.js
+++ assets/menu_data.js
@@ -1,21 +1,21 @@
 // 1. 首页-快捷服务 (Quick Actions)
 window.QUICK_ACTIONS = [
-    { id: 1, title: "航班动态", icon: "flight_takeoff", link: "#" },
-    { id: 2, title: "值机选座", icon: "airline_seat_recline_normal", link: "#" },
-    { id: 3, title: "机场大屏", icon: "monitor", link: "#" },
-    { id: 4, title: "行李查询", icon: "luggage", link: "#" },
-    { id: 5, title: "中转服务", icon: "connecting_airports", link: "#" },
-    { id: 6, title: "贵宾厅", icon: "diamond", link: "#" },
-    { id: 7, title: "餐饮购物", icon: "restaurant", link: "#" },
-    { id: 8, title: "交通指南", icon: "directions_bus", link: "#" }
+    { id: 1, title: "航班信息", icon: "flight_takeoff", link: "/flights" },
+    { id: 2, title: "航班动态", icon: "connecting_airports", link: "/flights_map" },
+  //  { id: 3, title: "机场大屏", icon: "monitor", link: "#" },
+  //  { id: 4, title: "行李查询", icon: "luggage", link: "#" },
+  //  { id: 5, title: "中转服务", icon: "connecting_airports", link: "#" },
+  //  { id: 6, title: "贵宾厅", icon: "diamond", link: "#" },
+  //  { id: 7, title: "餐饮购物", icon: "restaurant", link: "#" },
+  //  { id: 8, title: "交通指南", icon: "directions_bus", link: "#" }
   ];
   
   // 2. 侧边栏-导航 (Navigation Drawer)
   window.SIDEBAR_ITEMS = [
     { id: 101, title: "首页", icon: "home", link: "index.html" },
-    { id: 102, title: "我的行程", icon: "calendar_month", link: "#" },
-    { id: 103, title: "会员中心", icon: "account_circle", link: "#" },
-    { id: 104, title: "消息通知", icon: "notifications", link: "#" },
+   // { id: 102, title: "我的行程", icon: "calendar_month", link: "#" },
+   // { id: 103, title: "会员中心", icon: "account_circle", link: "#" },
+   // { id: 104, title: "消息通知", icon: "notifications", link: "#" },
     { id: 105, title: "设置", icon: "settings", link: "#" }, // 逻辑上我们会拦截这个点击打开弹窗
-    { id: 106, title: "关于我们", icon: "info", link: "#" }
+    { id: 106, title: "关于", icon: "info", link: "#" }
   ];
