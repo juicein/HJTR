6d8909f6f9b1e0241526de23fa338e00fa624b71
juicein <77275840+juicein@users.noreply.github.com>
2026-06-22 11:30:31 +0700

    更新 script.js

--- bus/script.js
+++ bus/script.js
@@ -629,7 +629,7 @@ function openDetail(line) {
   /* --- 注意注意！！！！颜色修改喵 自动颜色匹配修复 --- */
     /* ---const badgeColor = line.color ? `#${line.color.slice(0,6)}` : 'var(--md-sys-color-primary)';--- */
 const badgeColor = line.color
-  ? `#${line.color.replace(/^#/, '').slice(0, 6)}`
+  ? `#${line.color.replace(/^#+/, '')}`
   : 'var(--md-sys-color-primary)';
 
   header.innerHTML = `
